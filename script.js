f1d2106d0f8c717054e8472610ee1815686370bd
rainingingerale <michaeltgibbons@gmail.com>
2018-03-03 10:21:01 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -6,143 +6,119 @@ var getMovie4 = document.getElementById("movieKey4");
 var getMovie5 = document.getElementById("movieKey5");
 var getMovie6 = document.getElementById("movieKey6");
 var getMovie7 = document.getElementById("movieKey7");
-
 var getMovie8 = document.getElementById("movieKey8");
 var getMovie9 = document.getElementById("movieKey9");
 var getMovie10 = document.getElementById("movieKey10");
 var getMovie11 = document.getElementById("movieKey11");
 var getMovie12 = document.getElementById("movieKey12");
-
 var getMovie13 = document.getElementById("movieKey13");
 var getMovie14 = document.getElementById("movieKey14");
 var getMovie15 = document.getElementById("movieKey15");
 var getMovie16 = document.getElementById("movieKey16");
 var getMovie17 = document.getElementById("movieKey17");
-
 var getMovie18 = document.getElementById("movieKey18");
 var getMovie19 = document.getElementById("movieKey19");
 var getMovie20 = document.getElementById("movieKey20");
 var getMovie21 = document.getElementById("movieKey21");
 var getMovie22 = document.getElementById("movieKey22");
-
 var getMovie23 = document.getElementById("movieKey23");
 var getMovie24 = document.getElementById("movieKey24");
 var getMovie25 = document.getElementById("movieKey25");
 var getMovie26 = document.getElementById("movieKey26");
 var getMovie27 = document.getElementById("movieKey27");
-
 var getMovie28 = document.getElementById("movieKey28");
 var getMovie29 = document.getElementById("movieKey29");
 var getMovie30 = document.getElementById("movieKey30");
 var getMovie31 = document.getElementById("movieKey31");
 var getMovie32 = document.getElementById("movieKey32");
-
 var getMovie33 = document.getElementById("movieKey33");
 var getMovie34 = document.getElementById("movieKey34");
 var getMovie35 = document.getElementById("movieKey35");
 var getMovie36 = document.getElementById("movieKey36");
 var getMovie37 = document.getElementById("movieKey37");
-
 var getMovie38 = document.getElementById("movieKey38");
 var getMovie39 = document.getElementById("movieKey39");
 var getMovie40 = document.getElementById("movieKey40");
 var getMovie41 = document.getElementById("movieKey41");
 var getMovie42 = document.getElementById("movieKey42");
-
 var getMovie43 = document.getElementById("movieKey43");
 var getMovie44 = document.getElementById("movieKey44");
 var getMovie45 = document.getElementById("movieKey45");
 var getMovie46 = document.getElementById("movieKey46");
 var getMovie47 = document.getElementById("movieKey47");
-
 var getMovie48 = document.getElementById("movieKey48");
 var getMovie49 = document.getElementById("movieKey49");
 var getMovie50 = document.getElementById("movieKey50");
 var getMovie51 = document.getElementById("movieKey51");
 var getMovie52 = document.getElementById("movieKey52");
-
 var getMovie53 = document.getElementById("movieKey53");
 var getMovie54 = document.getElementById("movieKey54");
 var getMovie55 = document.getElementById("movieKey55");
 var getMovie56 = document.getElementById("movieKey56");
 var getMovie57 = document.getElementById("movieKey57");
-
 var getMovie58 = document.getElementById("movieKey58");
 var getMovie59 = document.getElementById("movieKey59");
 var getMovie60 = document.getElementById("movieKey60");
 var getMovie61 = document.getElementById("movieKey61");
 var getMovie62 = document.getElementById("movieKey62");
-
 var getMovie63 = document.getElementById("movieKey63");
 var getMovie64 = document.getElementById("movieKey64");
 var getMovie65 = document.getElementById("movieKey65");
 var getMovie66 = document.getElementById("movieKey66");
 var getMovie67 = document.getElementById("movieKey67");
-
 var getMovie68 = document.getElementById("movieKey68");
 var getMovie69 = document.getElementById("movieKey69");
 var getMovie70 = document.getElementById("movieKey70");
 var getMovie71 = document.getElementById("movieKey71");
 var getMovie72 = document.getElementById("movieKey72");
-
 var getMovie73 = document.getElementById("movieKey73");
 var getMovie74 = document.getElementById("movieKey74");
 var getMovie75 = document.getElementById("movieKey75");
 var getMovie76 = document.getElementById("movieKey76");
 var getMovie77 = document.getElementById("movieKey77");
-
 var getMovie78 = document.getElementById("movieKey78");
 var getMovie79 = document.getElementById("movieKey79");
 var getMovie80 = document.getElementById("movieKey80");
 var getMovie81 = document.getElementById("movieKey81");
 var getMovie82 = document.getElementById("movieKey82");
-
 var getMovie83 = document.getElementById("movieKey83");
 var getMovie84 = document.getElementById("movieKey84");
 var getMovie85 = document.getElementById("movieKey85");
 var getMovie86 = document.getElementById("movieKey86");
 var getMovie87 = document.getElementById("movieKey87");
-
 var getMovie88 = document.getElementById("movieKey88");
 var getMovie89 = document.getElementById("movieKey89");
 var getMovie90 = document.getElementById("movieKey90");
 var getMovie91 = document.getElementById("movieKey91");
 var getMovie92 = document.getElementById("movieKey92");
-
 var getMovie93 = document.getElementById("movieKey93");
 var getMovie94 = document.getElementById("movieKey94");
 var getMovie95 = document.getElementById("movieKey95");
 var getMovie96 = document.getElementById("movieKey96");
 var getMovie97 = document.getElementById("movieKey97");
-
 var getMovie98 = document.getElementById("movieKey98");
 var getMovie99 = document.getElementById("movieKey99");
 var getMovie100 = document.getElementById("movieKey100");
 var getMovie101 = document.getElementById("movieKey101");
 var getMovie102 = document.getElementById("movieKey102");
-
 var getMovie103 = document.getElementById("movieKey103");
 var getMovie104 = document.getElementById("movieKey104");
 var getMovie105 = document.getElementById("movieKey105");
 var getMovie106 = document.getElementById("movieKey106");
 var getMovie107 = document.getElementById("movieKey107");
-
 var getMovie108 = document.getElementById("movieKey108");
 var getMovie109 = document.getElementById("movieKey109");
 var getMovie110 = document.getElementById("movieKey110");
 var getMovie111 = document.getElementById("movieKey111");
 var getMovie112 = document.getElementById("movieKey112");
-
 var getMovie113 = document.getElementById("movieKey113");
 var getMovie114 = document.getElementById("movieKey114");
 var getMovie115 = document.getElementById("movieKey115");
 var getMovie116 = document.getElementById("movieKey116");
 var getMovie117 = document.getElementById("movieKey117");
-
 var getMovie118 = document.getElementById("movieKey118");
 var getMovie119 = document.getElementById("movieKey119");
 var getMovie120 = document.getElementById("movieKey120");
-
 getMovie0.addEventListener("click", clickedMovie);
 getMovie1.addEventListener("click", clickedMovie);
 getMovie2.addEventListener("click", clickedMovie);
@@ -151,148 +127,130 @@ getMovie4.addEventListener("click", clickedMovie);
 getMovie5.addEventListener("click", clickedMovie);
 getMovie6.addEventListener("click", clickedMovie);
 getMovie7.addEventListener("click", clickedMovie);
-
 getMovie8.addEventListener("click", clickedMovie);
 getMovie9.addEventListener("click", clickedMovie);
 getMovie10.addEventListener("click", clickedMovie);
 getMovie11.addEventListener("click", clickedMovie);
 getMovie12.addEventListener("click", clickedMovie);
-
 getMovie13.addEventListener("click", clickedMovie);
 getMovie14.addEventListener("click", clickedMovie);
 getMovie15.addEventListener("click", clickedMovie);
 getMovie16.addEventListener("click", clickedMovie);
 getMovie17.addEventListener("click", clickedMovie);
-
 getMovie18.addEventListener("click", clickedMovie);
 getMovie19.addEventListener("click", clickedMovie);
 getMovie20.addEventListener("click", clickedMovie);
 getMovie21.addEventListener("click", clickedMovie);
 getMovie22.addEventListener("click", clickedMovie);
-
 getMovie23.addEventListener("click", clickedMovie);
 getMovie24.addEventListener("click", clickedMovie);
 getMovie25.addEventListener("click", clickedMovie);
 getMovie26.addEventListener("click", clickedMovie);
 getMovie27.addEventListener("click", clickedMovie);
-
 getMovie28.addEventListener("click", clickedMovie);
 getMovie29.addEventListener("click", clickedMovie);
 getMovie30.addEventListener("click", clickedMovie);
 getMovie31.addEventListener("click", clickedMovie);
 getMovie32.addEventListener("click", clickedMovie);
-
 getMovie33.addEventListener("click", clickedMovie);
 getMovie34.addEventListener("click", clickedMovie);
 getMovie35.addEventListener("click", clickedMovie);
 getMovie36.addEventListener("click", clickedMovie);
 getMovie37.addEventListener("click", clickedMovie);
-
 getMovie38.addEventListener("click", clickedMovie);
 getMovie39.addEventListener("click", clickedMovie);
 getMovie40.addEventListener("click", clickedMovie);
 getMovie41.addEventListener("click", clickedMovie);
 getMovie42.addEventListener("click", clickedMovie);
-
 getMovie43.addEventListener("click", clickedMovie);
 getMovie44.addEventListener("click", clickedMovie);
 getMovie45.addEventListener("click", clickedMovie);
 getMovie46.addEventListener("click", clickedMovie);
 getMovie47.addEventListener("click", clickedMovie);
-
 getMovie48.addEventListener("click", clickedMovie);
 getMovie49.addEventListener("click", clickedMovie);
 getMovie50.addEventListener("click", clickedMovie);
 getMovie51.addEventListener("click", clickedMovie);
 getMovie52.addEventListener("click", clickedMovie);
-
 getMovie53.addEventListener("click", clickedMovie);
 getMovie54.addEventListener("click", clickedMovie);
 getMovie55.addEventListener("click", clickedMovie);
 getMovie56.addEventListener("click", clickedMovie);
 getMovie57.addEventListener("click", clickedMovie);
-
 getMovie58.addEventListener("click", clickedMovie);
 getMovie59.addEventListener("click", clickedMovie);
 getMovie60.addEventListener("click", clickedMovie);
 getMovie61.addEventListener("click", clickedMovie);
 getMovie62.addEventListener("click", clickedMovie);
-
 getMovie63.addEventListener("click", clickedMovie);
 getMovie64.addEventListener("click", clickedMovie);
 getMovie65.addEventListener("click", clickedMovie);
 getMovie66.addEventListener("click", clickedMovie);
 getMovie67.addEventListener("click", clickedMovie);
-
 getMovie68.addEventListener("click", clickedMovie);
 getMovie69.addEventListener("click", clickedMovie);
 getMovie70.addEventListener("click", clickedMovie);
 getMovie71.addEventListener("click", clickedMovie);
 getMovie72.addEventListener("click", clickedMovie);
-
 getMovie73.addEventListener("click", clickedMovie);
 getMovie74.addEventListener("click", clickedMovie);
 getMovie75.addEventListener("click", clickedMovie);
 getMovie76.addEventListener("click", clickedMovie);
 getMovie77.addEventListener("click", clickedMovie);
-
 getMovie78.addEventListener("click", clickedMovie);
 getMovie79.addEventListener("click", clickedMovie);
 getMovie80.addEventListener("click", clickedMovie);
 getMovie81.addEventListener("click", clickedMovie);
 getMovie82.addEventListener("click", clickedMovie);
-
 getMovie83.addEventListener("click", clickedMovie);
 getMovie84.addEventListener("click", clickedMovie);
 getMovie85.addEventListener("click", clickedMovie);
 getMovie86.addEventListener("click", clickedMovie);
 getMovie87.addEventListener("click", clickedMovie);
-
 getMovie88.addEventListener("click", clickedMovie);
 getMovie89.addEventListener("click", clickedMovie);
 getMovie90.addEventListener("click", clickedMovie);
 getMovie91.addEventListener("click", clickedMovie);
 getMovie92.addEventListener("click", clickedMovie);
-
 getMovie93.addEventListener("click", clickedMovie);
 getMovie94.addEventListener("click", clickedMovie);
 getMovie95.addEventListener("click", clickedMovie);
 getMovie96.addEventListener("click", clickedMovie);
 getMovie97.addEventListener("click", clickedMovie);
-
 getMovie98.addEventListener("click", clickedMovie);
 getMovie99.addEventListener("click", clickedMovie);
 getMovie100.addEventListener("click", clickedMovie);
 getMovie101.addEventListener("click", clickedMovie);
 getMovie102.addEventListener("click", clickedMovie);
-
 getMovie103.addEventListener("click", clickedMovie);
 getMovie104.addEventListener("click", clickedMovie);
 getMovie105.addEventListener("click", clickedMovie);
 getMovie106.addEventListener("click", clickedMovie);
 getMovie107.addEventListener("click", clickedMovie);
-
 getMovie108.addEventListener("click", clickedMovie);
 getMovie109.addEventListener("click", clickedMovie);
 getMovie110.addEventListener("click", clickedMovie);
 getMovie111.addEventListener("click", clickedMovie);
 getMovie112.addEventListener("click", clickedMovie);
-
 getMovie113.addEventListener("click", clickedMovie);
 getMovie114.addEventListener("click", clickedMovie);
 getMovie115.addEventListener("click", clickedMovie);
 getMovie116.addEventListener("click", clickedMovie);
 getMovie117.addEventListener("click", clickedMovie);
-
 getMovie118.addEventListener("click", clickedMovie);
 getMovie119.addEventListener("click", clickedMovie);
 getMovie120.addEventListener("click", clickedMovie);
+
+var lockButtonElement = document.getElementById("lockInButton");
+lockButtonElement.addEventListener("click", lockInFunction);
+
 
 $(document).ready(function(){
   $("#button").click(function(){
       $("#hide").toggle();
   });
 });
+
 
 window.onload = function() {
   if(!localStorage.getItem('movieKey0')) {
@@ -303,15 +261,19 @@ window.onload = function() {
   }
 }
 
+
 function setStyles() {
-  for (i = 0; i < 120; i++){
+  for (i = 0; i < 121; i++){
    if (localStorage.getItem('movieKey' + i) == 'picked') {
     document.getElementById('movieKey' + i).className = "col picked";}
+    else if (localStorage.getItem('movieKey' + i) == 'locked') {
+      document.getElementById('movieKey' + i).className = "col locked";}
   }
 }
 
+
 function populateStorage() {
-  for (i = 0; i < 120; i++){
+  for (i = 0; i < 121; i++){
     localStorage.setItem("movieKey" + i, 'notPicked')
   }
 }
@@ -320,40 +282,49 @@ function populateStorage() {
 function clickedMovie() {
   var unPickedElements = $(this).siblings("div");
   var unPickedKeys = [];
-
   $(this).siblings().each(
     function (idx, el) {
     unPickedKeys.push(el.id);
   });
-
-  console.log(unPickedKeys)
-
   function changeStyles(x) {
-
-    if (x.className == "col notPicked") {
+    for (i=0; i < unPickedElements.length; i++) {
+      if (localStorage.getItem(unPickedKeys[i]) == 'locked') {
+      return;
+    }
+  }
+    if (localStorage.getItem(x.id) == "locked") {
+      return;
+    }
+    else if (x.className == "col notPicked") {
       x.className = "col picked"
       localStorage.setItem(x.id, 'picked')
-
-
-      for (var i=0; i < unPickedElements.length; i++) {
+      for (i=0; i < unPickedElements.length; i++) {
         unPickedElements[i].className = "col notPicked";
       }
-
-      for (var i=0; i < unPickedElements.length; i++) {
+      for (i=0; i < unPickedElements.length; i++) {
         localStorage.setItem(unPickedKeys[i], 'notPicked');
       }
     }
-
     else {
       x.className = "col notPicked";
       localStorage.setItem(x.id, 'notPicked');
-
     }
   };
-
   changeStyles(this);
-
 }
+
+
+function lockInFunction() {
+  var r = confirm("Are you sure you want to lock in your current picks?");
+  if (r == true) {
+  for (i = 0; i < 121; i++)
+   if (localStorage.getItem("movieKey" + i) == "picked")
+   localStorage.setItem("movieKey" + i, 'locked');
+   setStyles();
+  }
+}
+
+
 
 
 
